1c41aedaf936a2c614b3545e3644f46aac568d4a
danholder <46631159+danholder@users.noreply.github.com>
2019-01-13 15:16:34 +0000

    Initial commit

---
+++ app/src/main/java/com/example/daniel/firstandroidapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.daniel.firstandroidapp;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void displayMessage(View view) {
+        String name = ((EditText) findViewById(R.id.editText)).getText().toString();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Hello: " + name)
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // FIRE ZE MISSILES!
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+        // Create the AlertDialog object and return it
+        builder.create().show();
+    }
+}
